3a0521d0b3d97803257eb1409b620af99888c30c
Mariana Medeiros <mariana_medeiros.santos@outlook.com>
2025-04-15 09:54:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(18,"Mariana","mariana@hotmail.com",new Date(),1500.0, obj);
 		
 		System.out.println(obj);
-
+		System.out.println(seller);
 	}
 
 }
